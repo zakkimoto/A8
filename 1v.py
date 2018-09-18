da095f16751bff6cad6045702b239af188411202
Zakarias Fridriksson <zakariasfridriksson@Zakariass-MacBook-Pro.local>
2018-09-18 09:27:30 +0000

    adda inn fyrsta direction

--- 1v.py
+++ 1v.py
@@ -2,6 +2,8 @@
 
 print("you start on 1,1 you can go N")
 initial_in = input("input your direction")
-initial_in =
 
-if initial_in != "N"
+
+if initial_in.upper() != "N":
+    print("wrong direction")
+    
